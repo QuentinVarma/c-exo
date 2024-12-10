855a180a792a87b3c1b33e66a132f17242544645
QuentinVarma <quentin.varma@lycee-jeanrostand.fr>
2024-12-10 08:53:50 +0000

    exo.cpp/exo3.cpp

--- exo.cpp/exo3.cpp
+++ exo.cpp/exo3.cpp
@@ -1,30 +1,36 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
 int main() {
-    std::string operation;
+    string operation;
     double operande1, operande2;
 
-    // Demander à l'utilisateur d'entrer une opération arithmétique suivie de deux opérandes
-    std::cout << "Entrez une opération (+, -, *, /) suivie de deux valeurs séparées par un espace ! ";
-    std::cin >> operation >> operande1 >> operande2;
+    
+    cout << "Entrez une opération (+, -, *, /) suivie de deux valeurs séparées par un espace ! ";
+    cin >> operation >> operande1 >> operande2;
 
-    // Utiliser une série d'instructions if pour déterminer l'opération demandée par l'utilisateur
+    
     if (operation == "+") {
-        std::cout << operande1 << "+" << operande2 << " = " << operande1 + operande2 << std::endl;
+        cout << operande1 << "+" << operande2 << " = " << operande1 + operande2 << endl;
     } else if (operation == "-") {
-        std::cout << operande1 << "-" << operande2 << " = " << operande1 - operande2 << std::endl;
+        cout << operande1 << "-" << operande2 << " = " << operande1 - operande2 << endl;
     } else if (operation == "*") {
-        std::cout << operande1 << "*" << operande2 << " = " << operande1 * operande2 << std::endl;
+        cout << operande1 << "*" << operande2 << " = " << operande1 * operande2 << endl;
     } else if (operation == "/") {
         if (operande2 != 0) {
-            std::cout << operande1 << "/" << operande2 << " = " << operande1 / operande2 << std::endl;
+            cout << operande1 << "/" << operande2 << " = " << operande1 / operande2 << endl;
         } else {
-            std::cout << "Attention division par zéro" << std::endl;
+            cout << "Attention division par zéro" << endl;
         }
     } else {
-        std::cout << "Erreur : opérateur inconnu " << operation << std::endl;
+        cout << "Erreur : opérateur inconnu " << operation << endl;
     }
 
     return 0;
 }
+
+
+
+
+
